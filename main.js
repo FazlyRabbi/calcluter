c0fbc80be6c1c11418af314837287ca4161b1cdd
FazlyRabbi <rabbisa74@gmail.com>
2019-08-18 07:11:57 +0600

    fixed some bugs of my calcluter

--- main.js
+++ main.js
@@ -1,36 +1,24 @@
-
-
 //adding event in btn 
 document.querySelector("#btn").addEventListener("click", function () {
-
     const person = document.querySelector("#name").value;
     const quantity = document.querySelector("#quantity").value;
     const kg = document.querySelector("#kg").value;
     const gm = document.querySelector("#gm").value;
-
     const profile = new Sum(person, quantity, kg, gm);
-
     const ui = new Ui();
-
     if (quantity === "" || kg === "" || gm === "") {
-
         ui.showMsg("দয়া করে প্রয়োজনীয় তথ্য প্রদান করুন", "danger");
-
     } else {
         ui.addItem(profile);
         ui.showMsg("আপনার তথ্য যুক্ত হয়েছে", "success");
-        ui.clearField();
-
-
-
-
+        ui.clearField()
     }
 
 })
 
-
-
+//select button and add event 
 document.querySelector(".btn2").addEventListener("click", () => {
+    //instensite the sum and ui class 
     const sum = new Sum();
     const ui = new Ui();
 
@@ -41,15 +29,9 @@ document.querySelector(".btn2").addEventListener("click", () => {
         sum.cal();
         ui.clearField();
     }
-
-
 });
 
-
-
 // adding click event in rest button 
-
-
 document.querySelector(".btn3").addEventListener("click", () => {
     const ui = new Ui();
     ui.reset();
--- ui.js
+++ ui.js
@@ -1,8 +1,4 @@
-
-
-
 class Ui {
-
     // add tiem 
     addItem({
         name,
@@ -11,35 +7,27 @@ class Ui {
         gm
     }) {
 
-        
         const person = document.querySelector("#person");
-
         person.textContent = name.toUpperCase();
-
         let tbody = document.querySelector("#tbody");
-
         const tr = document.createElement("tr");
-
         tr.innerHTML = `<td><span>${quantity}</span> পিছ</td><td><span>${kg}</span> কেজি</td><td><span>${gm}</span> গ্রাম</td>`
-
         tbody.appendChild(tr);
     }
-
+    // show massege in ui
     showMsg(massage, className) {
-
         const div = document.createElement("div");
         div.className = `alert alert-${className}`;
         div.textContent = massage;
         const jumbotron = document.querySelector(".jumbotron");
         const form = document.querySelector("form");
         jumbotron.insertBefore(div, form);
-
         setTimeout(() => {
             document.querySelector(".alert").remove();
         }, 2000);
     }
+    // clear all value frome input field 
     clearField() {
-
         document.querySelector("#name").value = ""
         document.querySelector("#quantity").value = ""
         document.querySelector("#kg").value = ""
@@ -47,7 +35,7 @@ class Ui {
         document.querySelector("#rate").value = "";
         document.querySelector("#name").setAttribute("disabled", "disabled");
     }
-
+    // clear all value frome all input field and table rows 
     reset() {
         document.querySelector("#pices").textContent = "";
         document.querySelector("#kgg").textContent = "";
@@ -59,29 +47,6 @@ class Ui {
         document.querySelector("#rate").value = "";
         document.querySelector("#name").removeAttribute("disabled");
         document.querySelector(".totalAccounts").innerHTML = "";
-      
-       
-        // const tbody = document.getElementById("table").children[1].children.length;
-
-        document.getElementById("table").children[1].children[0].remove();
-
-
-        // const tbodyChild = document.getElementById("table").tBodies.item(0)
-        // const rows = tbody.childNodes;
-        // console.log(rows);
-        // console.log(tbody);
-
-        // for(let i = 0; i<= tbody; i++){
-
-        //     rows[i].remove();
-            
-        // }
-        // console.log(document.querySelector("#table").children[1].children[0]);
-        
-       
-        
+        document.getElementById("table").children[1].innerHTML = "";
     }
 }
-
-
-
